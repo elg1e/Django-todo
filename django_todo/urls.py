60766a08161904e8da6bbbf466c2aa6620f3e7bc
Dominic Elgie <domelgie94@gmail.com>
2020-01-10 19:48:29 +0000

    second commit

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import say_hello
 from todo.views import get_todo_list
 
 urlpatterns = [
