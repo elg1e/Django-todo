3ea4234b0f4a7ecbfece82f9c27a92e4b30c9261
Dominic Elgie <domelgie94@gmail.com>
2020-01-11 16:25:00 +0000

    create test_view.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Item
 
 class TestViews(TestCase):
     def test_get_home_page(self):
@@ -10,3 +11,15 @@ class TestViews(TestCase):
         page = self.client.get("/add")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_item_page(self):
+        item = Item(name='Create a Test')
+        item.save()
+        
+        page = self.client.get("/edit/{0}".format(item.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_page_for_item_that_does_not_exist(self):
+        page = self.client.get("/edit/1")
+        self.assertEqual(page.status_code, 404)
